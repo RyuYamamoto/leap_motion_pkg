aed2f0e0641aa789eef8187400778c5d6c711c03
RyuYamamoto <rottertarmenation.y11.r18@gmail.com>
2015-03-01 19:12:04 +0900

    update

--- leap_subscriber/src/leap_subscriber.cpp
+++ leap_subscriber/src/leap_subscriber.cpp
@@ -5,20 +5,20 @@ using namespace std;
 
 void call_back_leapdata(const leap_subscriber::leapros msg)
 {
-		cout<<"x:"<<msg.ypr.x<<" ";
-		cout<<"y:"<<msg.ypr.y<<" ";
-		cout<<"z:"<<msg.ypr.z<<endl;
+	cout<<"x:"<<msg.ypr.x<<" ";
+	cout<<"y:"<<msg.ypr.y<<" ";
+	cout<<"z:"<<msg.ypr.z<<endl;
 }
 
 int main(int argc,char **argv)
 {
-		ros::init(argc,argv,"leap_sub");
-		ros::NodeHandle n;
+	ros::init(argc,argv,"leap_sub");
+	ros::NodeHandle n;
 
-		leap_subscriber::leapros msgs;
-		ros::Subscriber leap_sub = n.subscribe("leapmotion/data",1000,call_back_leapdata);
-		ros::spin();
+	leap_subscriber::leapros msgs;
+	ros::Subscriber leap_sub = n.subscribe("leapmotion/data",1000,call_back_leapdata);
+	ros::spin();
 
-		return 0;
+	return 0;
 }
 
